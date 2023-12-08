7348dc4f5d4a4408e8cc25e1dce558a94b3f9524
Rafik006 <rafikmhadhbi01@gmail.com>
2023-12-08 15:48:05 +0100

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import  {Maps}  from './map'
+import  Maps  from './map'
 import * as React from 'react';
 
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
     <div style={{ width: "1000px", height : "100"}}>
 <Maps />
     </div>
-    
+
   )
 }
 
--- src/map.jsx
+++ src/map.jsx
@@ -1,60 +1,37 @@
-import React, { useEffect } from "react";
-import ReactMapGL, { Marker } from 'react-map-gl';
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
-import 'mapbox-gl/dist/mapbox-gl.css';
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+mapboxgl.accessToken = 'pk.eyJ1Ijoid29ya3NwYWNlODU0IiwiYSI6ImNsbDlnZHgxbzFmNmQzY2w3cnlteDF6cmQifQ.0mKeOtdHiEMHDyGzUef0fw';
 
- 
+export default function maps() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
 
-export const Maps =()=>{
-    const mapRef = React.useRef();
-    const [lat, setLat] = React.useState(36.8189700)
-    const [lng, setLng] = React.useState(10.1657900)
-
-    const [viewport, setViewport] = React.useState({
-      longitude:  lng||10.1657900,
-      latitude:  lat || 36.8189700,
-      zoom: 8,
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
     });
-    useEffect(()=>{
-        setViewport({
-            longitude:  10.1657900,
-            latitude:  lat || 36.8189700,
-            zoom: 8,
-          })    },[lat])
-    return(
-        <>
-             <ReactMapGL
-        ref={mapRef}
-        mapboxAccessToken={"pk.eyJ1Ijoid29ya3NwYWNlODU0IiwiYSI6ImNsbDlnZHgxbzFmNmQzY2w3cnlteDF6cmQifQ.0mKeOtdHiEMHDyGzUef0fw"}
-        {...viewport}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        onClick={(event) => {
-            const { lng, lat } = event.lngLat;
 
-           setLat(lat)
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
 
-        }}
-
-      >
-                <Marker
-        latitude={lat}
-        longitude={lng}
-        draggable
-        onDragEnd={(e) => {
-            console.log('data',e.lngLat)
-            setLat(e.lngLat.lat)
-            setLng(e.lngLat.lng)
-
-        }
-
-        }
-    />
-      </ReactMapGL>
-        </>
-
-    )
+  return (
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
 }
- 
- 
-// in render()
